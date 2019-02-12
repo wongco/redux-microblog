c5e0491147b19124c23728d969dab9a61a957fd0
wongco <ginson@gmail.com>
2019-02-11 23:56:37 -0800

    add font awesome and update vote styling

--- src/Components/Vote.js
+++ src/Components/Vote.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons';
 
 const StyledVoteContainer = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: space-around;
+  width: 7em;
   align-items: center;
-  padding: 20px 0;
+  padding: 1em 0;
 `;
 
 class Vote extends Component {
@@ -16,8 +20,16 @@ class Vote extends Component {
     return (
       <StyledVoteContainer>
         <div>{`${this.props.votes} votes`}</div>
-        <button onClick={this.handleUpVote}>UpVote</button>
-        <button onClick={this.handleDownVote}>DownVote</button>
+        <FontAwesomeIcon
+          style={{ color: 'midnightblue', cursor: 'pointer' }}
+          icon={faThumbsUp}
+          onClick={this.handleUpVote}
+        />
+        <FontAwesomeIcon
+          style={{ color: 'red', cursor: 'pointer' }}
+          icon={faThumbsDown}
+          onClick={this.handleDownVote}
+        />
       </StyledVoteContainer>
     );
   }
